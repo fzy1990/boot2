c2cb13f82111920661f4d3b8ae2ed5f88516e719
fzy1990 <513507038@qq.com>
2018-06-23 15:48:20 +0800

    删掉注解

--- src/main/java/com/example/boot2/Boot2Application.java
+++ src/main/java/com/example/boot2/Boot2Application.java
@@ -3,7 +3,6 @@ package com.example.boot2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Boot2Application {
 
     public static void main(String[] args) {
