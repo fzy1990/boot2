3e6f428447beda527017d306c99bc233988768d7
fzy1990 <513507038@qq.com>
2018-06-23 15:52:44 +0800

    添加注解

--- src/main/java/com/example/boot2/Boot2Application.java
+++ src/main/java/com/example/boot2/Boot2Application.java
@@ -3,8 +3,10 @@ package com.example.boot2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 @SpringBootApplication
 public class Boot2Application {
 
